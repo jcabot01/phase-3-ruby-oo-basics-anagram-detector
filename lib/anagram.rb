f636ef7702aa3f430f60a9f98bb6f11b409dd7da
JonCabot <jfc0053@gmail.com>
2022-03-19 20:05:28 -0500

    commit tree dcabe6577c084ae36870f742c9ed2ae07c9c0673

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+# require 'pry'
+
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  # take in array, filter the split characters (into single characters, via .split""), sort this data Alphabetically, and compare to the word parameter under the same circumstances
+  def match(array)
+    array.select {|x| x.split("").sort == @word.split("").sort}
+  end
+# binding.pry
+end
